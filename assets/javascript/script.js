cdefa5340a9e24ba1f81e54b8c970cb627ff4d17
Josh Jancula <josh@jancula.com>
2018-01-11 00:21:27 +0000

    better on ipad

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,34 +1,35 @@
  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBCogGYJgHq6qekiGKL3RZOM32tJmm3JMU",
-    authDomain: "webpagecontacts.firebaseapp.com",
-    databaseURL: "https://webpagecontacts.firebaseio.com",
-    projectId: "webpagecontacts",
-    storageBucket: "webpagecontacts.appspot.com",
-    messagingSenderId: "43556131936"
-  };
-  firebase.initializeApp(config);
+ var config = {
+   apiKey: "AIzaSyBCogGYJgHq6qekiGKL3RZOM32tJmm3JMU",
+   authDomain: "webpagecontacts.firebaseapp.com",
+   databaseURL: "https://webpagecontacts.firebaseio.com",
+   projectId: "webpagecontacts",
+   storageBucket: "webpagecontacts.appspot.com",
+   messagingSenderId: "43556131936"
+ };
+ firebase.initializeApp(config);
 
-  var database = firebase.database();
+ var database = firebase.database();
 
-  $("#submitMessage").on("click", function(event) {
-  event.preventDefault();
+ $("#submitMessage").on("click", function(event) {
+   event.preventDefault();
 
-  // Grabbed values from text boxes
-  name = $("#name").val().trim();
-  email = $("#email").val().trim();
-  message = $("#message").val().trim();
+   // Grabbed values from text boxes
+   var name = $("#name").val().trim();
+   var email = $("#email").val().trim();
+   var message = $("#message").val().trim();
 
-  // Code for handling the push
-  database.ref().push({
-    name: name,
-    email: email,
-    message: message,
-    dateAdded: firebase.database.ServerValue.TIMESTAMP
-  });
+   // Code for handling the push
+   database.ref().push({
+     name: name,
+     email: email,
+     message: message,
+     dateAdded: firebase.database.ServerValue.TIMESTAMP
+   });
 
-	$("#name").val("");
-	$("#email").val("");
-	$("#message").val("");
-      
-});
+   $("#name").val("");
+   $("#email").val("");
+   $("#message").val("");
+
+ });
+ 
